8d45c5f26c69d4ee0bec7dfd4a9eaa3a1dd90bd8
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-04-16 23:01:55 -0700

    removes unnecessary variable declaration

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,14 @@
-// adds a global variable for the projectBucket to handle click events
-const projectBucket = document.querySelector('#previousProjects');
-
 //adds function to click event within the projectBucket
-projectBucket.addEventListener('click', function (event) {
-  let selectedElement = event.target;
+document
+  .querySelector('#previousProjects')
+  .addEventListener('click', function (event) {
+    let selectedElement = event.target;
 
-  // if statement ensures that there is a div element in the click's path to the root of the DOM.  If a div exists, it uses the data-link attribute to open the selected link in a new tab.
-  if (selectedElement.closest('div')) {
-    window.open(
-      `${selectedElement.closest('div').getAttribute('data-link')}`,
-      '_blank'
-    );
-  }
-});
+    // if statement ensures that there is a div element in the click's path to the root of the DOM.  If a div exists, it uses the data-link attribute to open the selected link in a new tab.
+    if (selectedElement.closest('div')) {
+      window.open(
+        `${selectedElement.closest('div').getAttribute('data-link')}`,
+        '_blank'
+      );
+    }
+  });
