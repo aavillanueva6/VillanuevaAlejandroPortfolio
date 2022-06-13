8588a41e114100a709d4a924cedd26c21cf25ade
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-06-13 14:47:34 -0700

    adds section styling, beginning of contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
 //adds function to click event within the projectBucket
-document
-  .querySelector('#previousProjects')
-  .addEventListener('click', function (event) {
-    let selectedElement = event.target;
+// document
+//   .querySelector('#previousProjects')
+//   .addEventListener('click', function (event) {
+//     let selectedElement = event.target;
 
-    // if statement ensures that there is a div element in the click's path to the root of the DOM.  If a div exists, it uses the data-link attribute to open the selected link in a new tab.
-    if (selectedElement.closest('div')) {
-      window.open(
-        `${selectedElement.closest('div').getAttribute('data-link')}`,
-        '_blank'
-      );
-    }
-  });
+//     // if statement ensures that there is a div element in the click's path to the root of the DOM.  If a div exists, it uses the data-link attribute to open the selected link in a new tab.
+//     if (selectedElement.closest('div')) {
+//       window.open(
+//         `${selectedElement.closest('div').getAttribute('data-link')}`,
+//         '_blank'
+//       );
+//     }
+//   });
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 65 || document.documentElement.scrollTop > 65) {
+//     document.getElementById('scrollspy-nav').style.padding = '8px 0px';
+//     document.getElementById('AVLogo').style.fontSize = '25px';
+//   } else {
+//     document.getElementById('scrollspy-nav').style.padding = '80px 10px';
+//     document.getElementById('AVLogo').style.fontSize = '45px';
+//   }
+// }
